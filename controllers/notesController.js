df726da7f5eb7a33f4c255de72613370ffa7cb82
Billy Franz <billfranz00@gmail.com>
2017-05-19 20:43:42 -0700

    rendering data from mongodb and added break in loop for index

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -1,10 +1,10 @@
 // Require mongoose for connection to mongoDB and Body Parser for easy gathering of data from form 
 var mongoose = require('mongoose'),
 	bodyParser = require('body-parser'),
-	Note = require('../models/notesSchema')
+	Note = require('../models/notesSchema');
 
 // MongoDB connection
-// mongoose.connect('mongodb://localhost/noteTest')
+mongoose.connect('mongodb://localhost/noteTest')
 
 // Template for Notes
 // var notesSchema = new mongoose.Schema({
@@ -17,11 +17,11 @@ var mongoose = require('mongoose'),
 // var Note = mongoose.model('Note', notesSchema)
 
 // Dummy Data
-var data = [{title: 'Grocery List', note: 'Apples, bananas, chicken, mixed greens', date: 20170423},
-			{title: 'My Address', note: '69 two way Buttsville, NJ 07829', date: 20160525},
-			{title: 'My friends', note: 'Bobert, Sue, Jefferman93', date: 20140316},
-			{title: 'Things to get', note: 'Star Wars Hat, snowboard, NJ banner', date: 20170505},
-			{title: 'Weird Stuff', note: 'I love cats, except the one named santa paws', date: 57695769}];
+// var data = [{title: 'Grocery List', note: 'Apples, bananas, chicken, mixed greens', date: 20170423},
+// 			{title: 'My Address', note: '69 two way Buttsville, NJ 07829', date: 20160525},
+// 			{title: 'My friends', note: 'Bobert, Sue, Jefferman93', date: 20140316},
+// 			{title: 'Things to get', note: 'Star Wars Hat, snowboard, NJ banner', date: 20170505},
+// 			{title: 'Weird Stuff', note: 'I love cats, except the one named santa paws', date: 57695769}];
 
 var urlendcodedParser = bodyParser.urlencoded({extended: false})
 
@@ -30,7 +30,12 @@ var urlendcodedParser = bodyParser.urlencoded({extended: false})
 module.exports = function(app) {
 	
 	app.get('/', function(req, res) {
-		res.render('index', {notes: data})
+		Note.find({}, function(err, data) {
+			if(err) throw err;
+			console.log(data)
+			res.render('index', {notes: data});
+		})
+		// res.render('index', {notes: data})
 	})
 
 	app.get('/noteList', function(req, res) {
